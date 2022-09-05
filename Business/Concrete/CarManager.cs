1edfd4b2931d7d6508691ab02fed3a3ba46120be
İbrahim Uyanık <ibrahimhakkiuyanik@outlook.com>
2022-09-05 04:55:45 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if(car.CarName.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Eklenemedi lütfen araba ismini en 2 karakter olacak şeklilde ve günlük fiyatı 0'dan büyük olacak şekilde giriniz.");
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+    }
+}
