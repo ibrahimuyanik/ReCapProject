f623bcd64de2341141db021c81584134af9d87d5
İbrahim Uyanık <ibrahimhakkiuyanik@outlook.com>
2022-09-07 08:26:35 +0300

    10. Gün Ödev 1

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi";
+        public static string BrandAdded = "Marka eklendi";
+        public static string ColorAdded = "Renk eklendi";
+        public static string CarsListed = "Arabalar listelendi";
+    }
+}
